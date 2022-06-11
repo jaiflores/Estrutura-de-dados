9d9f2dbf97cb847521ede4d08363587c6a1c6283
jaiflores <jaianny.vivian@gmail.com>
2022-06-11 07:52:03 -0300

    Primeiro desafio do mod 2

---
+++ script.js
@@ -0,0 +1,16 @@
+var nome1 = "Jaianny";
+console.log(nome1);
+
+let idade1 = 30;
+console.log(idade1);
+
+let number = 2022;
+console.log(number-idade1);
+
+let soma = "2022";
+console.log(number+idade1);
+
+console.log("Estou mexendo no console.log");
+
+let quartaFeira = true
+console.log(quartaFeira)
